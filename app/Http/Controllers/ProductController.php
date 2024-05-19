1eb36bf4fe87be919f5dd9d15a8a8ed43ab05997
Rayan Hossain <hossain_shohan1@live.com>
2024-05-19 19:11:46 +0600

    minor

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -118,7 +118,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return $product;
+        // return $product::with('product_tag');
     }
 
     /**
