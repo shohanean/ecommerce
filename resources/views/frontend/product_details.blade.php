ad06602b92b5299f2cbbcd47faf72f166053bb1a
Rayan Hossain <hossain_shohan1@live.com>
2025-07-28 13:51:29 +0600

    new slider implemented at product details page

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -3,6 +3,76 @@
 @section('description')
     {{ $product->name }}
 @endsection
+
+@section('header_scripts')
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <!-- Demo styles -->
+    <style>
+        .swiper {
+            width: 100%;
+            height: 100%;
+        }
+
+        .swiper-slide {
+            text-align: center;
+            font-size: 18px;
+            background: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .swiper-slide img {
+            display: block;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .swiper {
+            width: 100%;
+            height: 300px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .swiper-slide {
+            background-size: cover;
+            background-position: center;
+        }
+
+        .mySwiper2 {
+            height: 80%;
+            width: 100%;
+        }
+
+        .mySwiper {
+            height: 20%;
+            box-sizing: border-box;
+            padding: 10px 0;
+        }
+
+        .mySwiper .swiper-slide {
+            width: 25%;
+            height: 100%;
+            opacity: 0.4;
+        }
+
+        .mySwiper .swiper-slide-thumb-active {
+            opacity: 1;
+        }
+
+        .swiper-slide img {
+            display: block;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    </style>
+@endsection
+
 @section('content')
     <!-- Breadcrumb area Start -->
     <div class="breadcrumb-area pt--70 pt-md--25">
@@ -137,8 +207,30 @@
                             @endisset
                         </div>
                     </div>
-                    <div class="col-md-6 product-main-image">
-                        <h1>asd</h1>
+                    <div class="col-md-6">
+                        <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff"
+                            class="swiper mySwiper2">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <img src="{{ $product->secondary_image }}" />
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="{{ $product->primary_image }}" />
+                                </div>
+                            </div>
+                            <div class="swiper-button-next"></div>
+                            <div class="swiper-button-prev"></div>
+                        </div>
+                        <div thumbsSlider="" class="swiper mySwiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <img src="{{ $product->secondary_image }}" />
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="{{ $product->primary_image }}" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-md-6 product-main-details mt--40 mt-md--10 mt-sm--30">
                         <div class="product-summary">
@@ -446,7 +538,26 @@
     <!-- Main Content Wrapper Start -->
 @endsection
 @section('footer_scripts')
+    <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script>
+        var swiper = new Swiper(".mySwiper", {
+            spaceBetween: 10,
+            slidesPerView: 4,
+            freeMode: true,
+            watchSlidesProgress: true,
+        });
+        var swiper2 = new Swiper(".mySwiper2", {
+            spaceBetween: 10,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            thumbs: {
+                swiper: swiper,
+            },
+        });
+
         $(document).ready(function() {
             $('#add_to_favourite').click(function() {
                 var product_id = "{{ $product->id }}";
